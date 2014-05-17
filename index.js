46de97fcb7a276f935338ee79063a324df8a8fb4
Rob Wierzbowski <hello@robwierzbowski.com>
2014-05-16 23:41:20 -0400

    Add more notes and comment out everything.

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ module.exports = function (options) {
 	var bundleExec = options.bundleExec;
 	var versionCommand = bundleExec === true ? 'bundle exec jekyll -v' : 'jekyll -v';
 
+	// Catch errors first.
+
 	try {
 		shelljs.exec(versionCommand, {silent: true});
 	}
@@ -25,6 +27,7 @@ module.exports = function (options) {
 	// NOTE: This looks at single files -- we need to look at the dir as a whole.
 	// NOTE: Should source matter / be applied to the input glob? No, let's just
 	// say that stream is source, output is dest
+	// NOTE: Only compiles site. Run doctor with node spawn or exec.
 
 	return through.obj(function (file, enc, cb) {
 		var self = this;
@@ -48,82 +51,84 @@ module.exports = function (options) {
 			}
 
 			// NOTE: set up real args. Let people pass anything except --dest && --source
-			var args = [
-				// 'sass',
-				// inputTempFile,
-				// outputTempFile,
-				// '--load-path', fileDirname
-			].concat(passedArgs);
+			// var args = [
+			// 	// 'sass',
+			// 	// inputTempFile,
+			// 	// outputTempFile,
+			// 	// '--load-path', fileDirname
+			// ].concat(passedArgs);
 
-			if (bundleExec) {
-				args.unshift('bundle', 'exec');
-			}
+			// if (bundleExec) {
+			// 	args.unshift('bundle', 'exec');
+			// }
 
 			// if we're compiling SCSS or CSS files
 			// if (path.extname(file.path) === '.css') {
 			// 	args.push('--scss');
 			// }
 
-			var cmd = args.shift();
-			var cp = spawn(cmd, args);
+			// var cmd = args.shift();
+			// var cp = spawn(cmd, args);
 
 			// NOTE: Will need this.
 			// if (process.argv.indexOf('--verbose') !== -1) {
 			// 	gutil.log('gulp-ruby-jekyll:', 'Running command:', cmd, chalk.blue(args.join(' ')));
 			// }
 
-			cp.on('error', function (err) {
-				// TODO: remove when gulp has fixed error handling
-				gutil.log('[gulp-ruby-jekyll] ' + err);
+			// NOTE: Internal error handling. Probably only need this once.
+			// cp.on('error', function (err) {
+			// 	// TODO: remove when gulp has fixed error handling
+			// 	gutil.log('[gulp-ruby-jekyll] ' + err);
 
-				self.emit('error', new gutil.PluginError('gulp-ruby-jekyll', err));
-				return cb();
-			});
+			// 	self.emit('error', new gutil.PluginError('gulp-ruby-jekyll', err));
+			// 	return cb();
+			// });
 
 			// NOTE: Setting up stderr output
-			var errors = '';
-			cp.stderr.setEncoding('utf8');
-			cp.stderr.on('data', function (data) {
-				// ignore deprecation and empty warnings
-				// if (/DEPRECATION WARNING/.test(data) || data.trim() === '') {
-				// 	return;
-				// }
+			// var errors = '';
+			// cp.stderr.setEncoding('utf8');
+			// cp.stderr.on('data', function (data) {
+			// 	// ignore deprecation and empty warnings
+			// 	// if (/DEPRECATION WARNING/.test(data) || data.trim() === '') {
+			// 	// 	return;
+			// 	// }
 
-				errors += data;
-			});
+			// 	errors += data;
+			// });
 
-			cp.on('close', function (code) {
-				if (errors) {
-					// TODO: remove when gulp has fixed error handling
-					// NOTE: Probably need to rewrite these two
-					// gutil.log('[gulp-ruby-jekyll]', '\n' + errors.replace(inputTempFile, file.path).replace('Use --trace for backtrace.\n', ''));
+			// NOTE: When it's all done
+			// cp.on('close', function (code) {
+			// 	if (errors) {
+			// 		// TODO: remove when gulp has fixed error handling
+			// 		// NOTE: Probably need to rewrite these two
+			// 		// gutil.log('[gulp-ruby-jekyll]', '\n' + errors.replace(inputTempFile, file.path).replace('Use --trace for backtrace.\n', ''));
 
-					// self.emit('error', new gutil.PluginError('gulp-ruby-jekyll', '\n' + errors.replace(inputTempFile, file.path).replace('Use --trace for backtrace.\n', '')));
-					return cb();
-				}
+			// 		// self.emit('error', new gutil.PluginError('gulp-ruby-jekyll', '\n' + errors.replace(inputTempFile, file.path).replace('Use --trace for backtrace.\n', '')));
+			// 		return cb();
+			// 	}
 
-				if (code > 0) {
-					self.emit('error', new gutil.PluginError('gulp-ruby-jekyll', 'Exited with error code ' + code));
-					return cb();
-				}
+			// 	if (code > 0) {
+			// 		self.emit('error', new gutil.PluginError('gulp-ruby-jekyll', 'Exited with error code ' + code));
+			// 		return cb();
+			// 	}
 
-				// NOTE: Pull in those new files
-				// NOTE: Once again, doing single files here, got to move to dirs
-				// fs.readFile(outputTempFile, function (err, data) {
-				// 	if (err) {
-				// 		self.emit('error', new gutil.PluginError('gulp-ruby-jekyll', err));
-				// 		return cb();
-				// 	}
+			// 	// NOTE: Pull in those new files
+			// 	// NOTE: Once again, doing single files here, got to move to dirs
+			// 	// fs.readFile(outputTempFile, function (err, data) {
+			// 	// 	if (err) {
+			// 	// 		self.emit('error', new gutil.PluginError('gulp-ruby-jekyll', err));
+			// 	// 		return cb();
+			// 	// 	}
 
-				// 	self.push(new gutil.File({
-				// 		base: fileDirname,
-				// 		contents: data
-				// 	}));
+			// 	// 	self.push(new gutil.File({
+			// 	// 		base: fileDirname,
+			// 	// 		contents: data
+			// 	// 	}));
 
-				// NOTE: final callback
-				// 	cb();
-				// });
-			});
+			// 	// NOTE: final callback
+			// 	// 	cb();
+			// 	// });
+			// });
 		});
 	});
 };
