0e988c24c18ad537b14b3f89d1a6422f665207b4
Rob Wierzbowski <hello@robwierzbowski.com>
2014-05-24 12:31:24 -0400

    Switch to implementing transform streams directly. YES!

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 'use strict';
 var fs = require('fs');
+var vfs = require('vinyl-fs');
 var path = require('path');
 var uuid = require('uuid');
 var gutil = require('gulp-util');
 var mkdirp = require('mkdirp');
-var through = require('through2');
 var osTempDir = require('os').tmpdir();
+var Transform = require('stream').Transform;
 
-// to use:
-// intermediate({ outputDir: 'some/relative/dir' }, function(){});
-
-module.exports = function (options, cb) {
+module.exports = function (options, process) {
   var tempDir = path.join(osTempDir, uuid.v4());
+  var parser = new Transform({ objectMode: true });
+  options.outputDir = options.outputDir ? path.join(tempDir, options.outputDir) : tempDir;
 
-  function writeIntermediateFiles(file, enc, cb) {
+  parser._transform = function(file, encoding, cb) {
     var self = this;
     var shortPath = path.relative(file.cwd, file.path);
     var intermediateFilePath = path.join(tempDir, shortPath);
@@ -30,8 +30,9 @@ module.exports = function (options, cb) {
     }
 
     mkdirp(intermediateFileDir, function (err) {
+
       if (err) {
-        self.emit('error', new gutil.PluginError('gulp-intermediate', 'err'));
+        self.emit('error', new gutil.PluginError('gulp-intermediate', err));
         return cb();
       }
 
@@ -44,12 +45,23 @@ module.exports = function (options, cb) {
         cb();
       });
     });
-  }
+  };
 
-  function processAndOutputFiles() {
-    cb();
-    // read options.outputdir back into stream
-  }
+  parser._flush = function(cb) {
+    var self = this;
 
-  return through.obj(writeIntermediateFiles, processAndOutputFiles);
+    // ALL RIGHT FOOLS, ADD SOME PROCESSING HERE
+
+    vfs.src(path.join(tempDir, '**/*')).on('data', function(file) {
+      // TODO: better way to filter directories?
+      if (file.contents) {
+        self.push(file);
+      }
+    })
+    .on('end', function() {
+      cb();
+    });
+  };
+
+  return parser;
 };
