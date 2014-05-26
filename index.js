448f7b606948b4d0a210e40e6161464d48b2b741
Rob Wierzbowski <hello@robwierzbowski.com>
2014-05-26 01:15:49 -0400

    WORKING STATE! Use vinyl and fs functions to write, read, and push out new files

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 'use strict';
+
 var fs = require('fs');
-var vfs = require('vinyl-fs');
 var path = require('path');
 var uuid = require('uuid');
+var File = require('vinyl');
+var glob = require('glob');
 var gutil = require('gulp-util');
 var mkdirp = require('mkdirp');
 var osTempDir = require('os').tmpdir();
 var Transform = require('stream').Transform;
 
+// use: intermediate({ outputDir: 'someDir' }, function(tempDir, cb) { actions; cb() })
+// TODO: Need better names for transform, process, shortpath & other vars
+
 module.exports = function (options, process) {
   var tempDir = path.join(osTempDir, uuid.v4());
-  var parser = new Transform({ objectMode: true });
-  options.outputDir = options.outputDir ? path.join(tempDir, options.outputDir) : tempDir;
+  var transform = new Transform({ objectMode: true });
+  var outputDir = options.outputDir ? path.join(tempDir, options.outputDir) : tempDir;
+  var origCWD;
+  var origBase;
 
-  parser._transform = function(file, encoding, cb) {
+  transform._transform = function(file, encoding, cb) {
     var self = this;
     var shortPath = path.relative(file.cwd, file.path);
     var intermediateFilePath = path.join(tempDir, shortPath);
     var intermediateFileDir = path.dirname(intermediateFilePath);
+
+    if (!origCWD){
+      origCWD = origCWD || file.cwd;
+    }
+
+    if (!origBase){
+      origBase = origBase || file.base;
+    }
 
     if (file.isNull()) {
       this.push(file);
@@ -29,6 +44,7 @@ module.exports = function (options, process) {
       return cb();
     }
 
+    // Can we cut this down?
     mkdirp(intermediateFileDir, function (err) {
 
       if (err) {
@@ -47,21 +63,42 @@ module.exports = function (options, process) {
     });
   };
 
-  parser._flush = function(cb) {
+  transform._flush = function(cb) {
     var self = this;
 
-    // ALL RIGHT FOOLS, ADD SOME PROCESSING HERE
+    process(tempDir, function() {
+      glob('**/*', { cwd: outputDir }, function (err, files) {
 
-    vfs.src(path.join(tempDir, '**/*')).on('data', function(file) {
-      // TODO: better way to filter directories?
-      if (file.contents) {
-        self.push(file);
-      }
-    })
-    .on('end', function() {
-      cb();
+        if (err) {
+          self.emit('error', new gutil.PluginError('gulp-intermediate', err));
+          return cb();
+        }
+
+        files.forEach(function (file) {
+          var realPath = path.join(outputDir, file);
+
+          if (fs.statSync(realPath).isFile()) {
+            fs.readFile(realPath, function(err, data) {
+
+              if (err) {
+                self.emit('error', new gutil.PluginError('gulp-intermediate', err));
+                return cb();
+              }
+
+              self.push( new File({
+                cwd: origCWD,
+                base: origBase,
+                path: path.join(origBase, file),
+                contents: new Buffer(data)
+              }));
+            });
+          }
+        });
+
+        cb();
+      });
     });
   };
 
-  return parser;
+  return transform;
 };
